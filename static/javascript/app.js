6db5ebe5aafa9fbd157bef11cb6e84e760187fa7
LevanNamicheishvili <117360031+LevanNamicheishvili@users.noreply.github.com>
2023-06-27 01:29:39 +0400

    Add files via upload

---
+++ static/javascript/app.js
@@ -0,0 +1,26 @@
+const rightside = document.getElementById("rigth_side_fetch");
+
+const loadProducts = () => {
+  const url = `../../data/data.json`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      data.fruits.forEach((fruit) => {
+        const card = document.createElement('div');
+        card.classList.add('card');
+        card.innerHTML = `
+          <div class="card_img_frame">
+            <img src="${fruit.img}">
+          </div>
+          <h2>${fruit.fruitname}</h2>
+          <p>${fruit.price}</p>
+        `;
+        rightside.appendChild(card);
+      });
+    })
+    .catch((error) => {
+      console.log(`Error fetching product data: ${error}`);
+    });
+};
+
+loadProducts();
